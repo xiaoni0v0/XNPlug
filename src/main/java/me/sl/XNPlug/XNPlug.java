19e0a798cd96608b15830e69fc1504a9518a43c9
XN <nyj2007@163.com>
2026-05-05 13:15:47 +0800

    init

---
+++ src/main/java/me/sl/XNPlug/XNPlug.java
@@ -0,0 +1,16 @@
+package me.sl.XNPlug;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XNPlug extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
